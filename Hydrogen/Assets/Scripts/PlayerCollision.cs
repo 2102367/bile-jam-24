a3c6f6c8058226e6192c86dabae7f541c0ad5146
Luca Bellu <luca.bellu1@gmail.com>
2024-03-30 15:22:52 +0000

    Added interactable sign and "jetpack" with fuel cost

--- Hydrogen/Assets/Scripts/PlayerCollision.cs
+++ Hydrogen/Assets/Scripts/PlayerCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using TarodevController;
 
 public class PlayerCollision : MonoBehaviour
 {
@@ -14,23 +15,44 @@ public class PlayerCollision : MonoBehaviour
     [SerializeField] private Slider hydrogenMeter;
     [SerializeField] private float hydrogenDecayMulti;
     private float hydrogenMeterValue;
+    [SerializeField] private float jetpackCostPerJump;
+    private bool isDraining = true;
+
+    private bool hasJetpack = false;
+    public ScriptableStats _jetPack;
 
     [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [SerializeField] private PlayerController playerControllerScript;
+
+    private SignText signText;
 
     private void Awake()
     {
         currentHydrogen = startHydrogen;
         interactText.gameObject.SetActive(false);
+        dialogueText.gameObject.SetActive(false);
+
     }
     private void Update()
     {
-        currentHydrogen -= hydrogenDecayMulti * Time.deltaTime;
+        if (isDraining)
+        {
+            currentHydrogen -= hydrogenDecayMulti * Time.deltaTime;
+        }
+
         hydrogenMeterValue = currentHydrogen / maxHydrogen; //convert current hydrogen to a number between 0-1 for slider
         hydrogenMeter.value = hydrogenMeterValue;
 
         if (currentHydrogen <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //restart scene on death
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) || hasJetpack) 
+        {
+            currentHydrogen -= jetpackCostPerJump;
         }
     }
 
@@ -39,13 +61,29 @@ public class PlayerCollision : MonoBehaviour
         if (collision.gameObject.CompareTag("hydrogen"))
         {
             currentHydrogen += hydrogenAdd;
+            if (currentHydrogen > maxHydrogen)
+            {
+                currentHydrogen = maxHydrogen;
+            }
             Destroy(collision.gameObject);
         }
 
-        if (collision.gameObject.CompareTag("door"))
+        if (collision.gameObject.CompareTag("door") || collision.gameObject.CompareTag("jetpack") || collision.gameObject.CompareTag("dialogue"))
         {
             //show popup
             interactText.gameObject.SetActive(true);
+        }
+
+        if (collision.gameObject.CompareTag("refill"))
+        {
+            isDraining = false;
+            currentHydrogen = maxHydrogen;
+        }
+
+        if (collision.gameObject.CompareTag("dialogue"))
+        {
+            signText = collision.gameObject.GetComponent<SignText>();
+            dialogueText.text = signText.text;
         }
     }
 
@@ -53,20 +91,50 @@ public class PlayerCollision : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("door"))
         {
-            print("on door");
             if (Input.GetKey(KeyCode.E))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //go to next level
+            }
+        }
+
+        if (collision.gameObject.CompareTag("jetpack"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                hasJetpack = true;
+                playerControllerScript._stats = _jetPack;
+                Destroy(collision.gameObject);
+            }
+        }
+
+        if (collision.gameObject.CompareTag("dialogue"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                //show dialogue
+                dialogueText.gameObject.SetActive(true);
+                interactText.gameObject.SetActive(false);
             }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("door"))
+        if (collision.gameObject.CompareTag("door") || collision.gameObject.CompareTag("jetpack") || collision.gameObject.CompareTag("dialogue"))
         {
             //remove popup
             interactText.gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.CompareTag("refill"))
+        {
+            isDraining = true;
+        }
+
+        if (collision.gameObject.CompareTag("dialogue"))
+        {
+            //hide dialogue
+            dialogueText.gameObject.SetActive(false);
         }
     }
 }
--- Hydrogen/Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
+++ Hydrogen/Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ namespace TarodevController
     [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class PlayerController : MonoBehaviour, IPlayerController
     {
-        [SerializeField] private ScriptableStats _stats;
+        public ScriptableStats _stats;
         private Rigidbody2D _rb;
         private CapsuleCollider2D _col;
         private FrameInput _frameInput;
@@ -190,7 +190,7 @@ namespace TarodevController
 #if UNITY_EDITOR
         private void OnValidate()
         {
-            if (_stats == null) Debug.LogWarning("Please assign a ScriptableStats asset to the Player Controller's Stats slot", this);
+            //if (_stats == null) Debug.LogWarning("Please assign a ScriptableStats asset to the Player Controller's Stats slot", this);
         }
 #endif
     }
