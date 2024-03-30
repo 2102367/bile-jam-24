8baf060d5fd122b7e0b8fb6d9989677c9d7c6f94
Luca Bellu <luca.bellu1@gmail.com>
2024-03-30 13:31:28 +0000

    Scene reloads on death, hydrogen bar

--- Hydrogen/Assets/Scripts/HydrogenCollision.cs
+++ Hydrogen/Assets/Scripts/HydrogenCollision.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HydrogenCollision : MonoBehaviour
 {
     [SerializeField] private float maxHydrogen;
     private float currentHydrogen;
+    [SerializeField] private float startHydrogen;
     [SerializeField] private float hydrogenAdd;
+    [SerializeField] private Slider hydrogenMeter;
+    [SerializeField] private float hydrogenDecayMulti;
+    private float hydrogenMeterValue;
 
     private void Awake()
     {
-        currentHydrogen = 10f;
+        currentHydrogen = startHydrogen;
     }
-
     private void Update()
     {
-        print(currentHydrogen);
+        currentHydrogen -= hydrogenDecayMulti * Time.deltaTime;
+        hydrogenMeterValue = currentHydrogen / maxHydrogen; //convert current hydrogen to a number between 0-1 for slider
+        hydrogenMeter.value = hydrogenMeterValue;
+        print(hydrogenMeterValue);
+
+        if (currentHydrogen <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
