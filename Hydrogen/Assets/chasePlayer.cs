bc8630f4ab3c2e60ae536389bc19e4ffe2ed5f93
denise <d.mills3@rgu.ac.uk>
2024-03-31 11:25:05 +0100

    ikik
    flips and inputs

--- Hydrogen/Assets/chasePlayer.cs
+++ Hydrogen/Assets/chasePlayer.cs
@@ -8,16 +8,19 @@ public class chasePlayer : MonoBehaviour
     public float speed;
     public float jumpForce;
     private Rigidbody2D enemyBody;
+    private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         enemyBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         Vector2 direction = player.transform.position - transform.position;
         float distance = direction.magnitude;
+        //Vector2 directionEnemy = enemyBody.velocity.normlized;
 
         // Move towards the player
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
@@ -28,6 +31,15 @@ public class chasePlayer : MonoBehaviour
         // {
         //     Jump();
         // }
+
+        if (direction.x > 0)
+        {
+            spriteRenderer.flipX = false; // Face right
+        }
+        else if (direction.x < 0)
+        {
+            spriteRenderer.flipX = true; // Face left
+        }
     }
 
     void Jump()
